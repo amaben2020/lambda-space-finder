ba363e290f5556915a280182785f19e8cb52955f
amaben2020 <60056927+amaben2020@users.noreply.github.com>
2025-02-12 10:02:52 +0100

    adding suffixer

--- src/stacks/DataStack.ts
+++ src/stacks/DataStack.ts
@@ -1,9 +1,21 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
+import { getSuffixFromStack } from '../utils';
 
-// this stack would hold the table
+// this stack would hold the dynamodb table
 export class DataStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const suffix = getSuffixFromStack(this);
+
+    new Table(this, 'SpacesTable', {
+      partitionKey: {
+        name: 'id',
+        type: AttributeType.STRING,
+      },
+      tableName: `SpaceTable-${suffix}`,
+    });
   }
 }
