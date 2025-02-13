4c1b859a07e1f25384d826b3e12b67697ea28b4f
amaben2020 <60056927+amaben2020@users.noreply.github.com>
2025-02-13 08:12:31 +0100

    removing uuid

--- src/services/hello.ts
+++ src/services/hello.ts
@@ -1,10 +1,8 @@
 import { APIGatewayProxyEvent, Context } from 'aws-lambda';
-import { v4 } from 'uuid';
 
 // we need to make this reusable in an ApiHandler wrapper that returns status and body correctly
 async function handler(event: APIGatewayProxyEvent, context: Context) {
   console.log(event);
-  console.log('uuid', v4);
 
   return {
     statusCode: 200,
